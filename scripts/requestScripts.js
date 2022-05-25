abe7fed8f88bec28f47c8371f5b02751638af1ac
Vinicius Goncalves <oldz.registros@gmail.com>
2022-05-25 19:58:50 -0300

    🔨 Add copy content, new layout, more colors, more updates

--- scripts/requestScripts.js
+++ scripts/requestScripts.js
@@ -3,45 +3,76 @@ const playlistIdSearch = document.querySelector('#playlist-id-search')
 const linksVideoContainer = document.querySelector('#links-video-container')
 const prevAndNextContainer = document.querySelector('#prev-next-container')
 const resultContainer = document.querySelector('#results-container')
+const headerInformation = document.querySelector('#header-information')
+const copy = document.querySelector('.copy')
+
+let playlistIdSearchInMemory = playlistIdSearch.value
 
 const url = (termToSearch) => `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=${termToSearch}&maxResults=50&key=AIzaSyAdUJVJ-T8RjlYjUMBzjCb181iFWcozD8Y`
+const pages = (pageToken) => `${url(playlistIdSearchInMemory)}&pageToken=${pageToken}`
 
+const copyContent = () => {
+    navigator
+    .clipboard
+    .writeText(Array.from(document.querySelectorAll('a')).join(', '))
+
+    headerInformation.textContent = "You've copied this page content succefully"
+    headerInformation.style.color = '#4adf4a'
+
+}
 
 const insertIntoHTML = extractResults => {
-    linksVideoContainer.innerHTML = extractResults.items.map((value, index) => {
+
+    prevAndNextContainer.innerHTML = ''
+    
+    linksVideoContainer.style.display = 'block'
+    extractButton.value = 'Extract again'
+    playlistIdSearch.value = ''
+    headerInformation.textContent = `The request has found ${extractResults.pageInfo.totalResults} videos from playlist: ${playlistIdSearchInMemory}`
+
+    linksVideoContainer.innerHTML = extractResults.items.map(value => {
         
         const videoId = value.contentDetails.videoId
         const videoFromIdToUrl = `https://www.youtube.com/watch?v=${videoId}`
-
+        
         return `
         <li class="video-link-extract-container">
-                    <a class="video-link-extract" href="${videoFromIdToUrl}" rel="external" target="_blank">${videoFromIdToUrl}</a>
+        <a class="video-link-extract" href="${videoFromIdToUrl}" rel="external" target="_blank">${videoFromIdToUrl}</a>
                     </li>`
     }).join('')
-
-    if(extractResults.nextPageToken || extractResults.prevPageToken) {
-        prevAndNextContainer.innerHTML = 
-        `${extractResults.prevPageToken ? `<button class="previous" onclick="fetchMoreResults('${extractResults.prevPageToken}')">Previous</button>` : ''}
+    
+    if (extractResults.nextPageToken || extractResults.prevPageToken) {
+        prevAndNextContainer.innerHTML =
+            `${extractResults.prevPageToken ? `<button class="previous" onclick="fetchMoreResults('${extractResults.prevPageToken}')">Previous</button>` : ''}
         ${extractResults.nextPageToken ? `<button class="next" onclick="fetchMoreResults('${extractResults.nextPageToken}')">More videos</button>` : ''}`
+    }
 
+    if (linksVideoContainer.querySelectorAll('li') != null) {
+        prevAndNextContainer.innerHTML += `<button class="copy" onclick="copyContent()">Copy Content Extracted</button>`
     }
 }
-
-const pages = (pageToken) => `${url(playlistIdSearch.value)}&pageToken=${pageToken}`
 
 const fetchMoreResults = async token => {
     const response = await fetch(pages(token))
     const data = await response.json()
     insertIntoHTML(data)
-} 
+}
 
-const fetchTerm = async () => {
-    const response = await fetch(url(playlistIdSearch.value))
+const fetchTerm = async playlistToExtract => {
+    const response = await fetch(url(playlistToExtract))
     const data = await response.json()
     insertIntoHTML(data)
 }
-    
 
 extractButton.addEventListener('click', () => {
-    fetchTerm()
+    const inputPlaylistId = playlistIdSearch.value.trim()
+    if (!inputPlaylistId) {
+        headerInformation.textContent = 'Please, insert a valid ID and try again.'
+        headerInformation.style.color = 'red'
+        return
+    }
+
+    playlistIdSearchInMemory = inputPlaylistId
+    fetchTerm(playlistIdSearchInMemory)
+
 })
